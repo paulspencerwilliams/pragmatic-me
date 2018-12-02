87dd6ea8bf3a6fe95620a18b70080e1652d7b48b
Paul Williams <paul@paulswilliams.me.uk>
2018-12-02 16:11:03 +0000

    Basic graphql query to populate title from site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Pragmatic me',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <h1>pragmatic me</h1>
+    <h1>{data.site.siteMetadata.title}</h1>
     <div>Some introductory blurb</div>
   </Layout>
 )
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
