5ce747c3b1882e460fc7231ff031e3e5539dfa68
Paul Williams <paul@paulswilliams.me.uk>
2018-12-18 17:50:04 +0000

    Upgrade gatsby-source-contentful and add @contentful/gatsby-transformer-contentful-richtext

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     title: 'Pragmatic me',
   },
   plugins: [
+    `@contentful/gatsby-transformer-contentful-richtext`,
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
