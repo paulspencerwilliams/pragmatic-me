a47f2c013b70f1665be5f5708b241d7c853e31f5
Paul Williams <paul@paulswilliams.me.uk>
2019-01-13 15:43:24 +0000

    Enable rich text entry links

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,29 @@
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
+const shortid = require('shortid')
+
+const { INLINES } = require('@contentful/rich-text-types')
 
 module.exports = {
   siteMetadata: {
     title: 'Pragmatic me',
   },
   plugins: [
-    `@contentful/gatsby-transformer-contentful-richtext`,
+    {
+      resolve: `@contentful/gatsby-transformer-contentful-richtext`,
+      options: {
+        renderOptions: {
+          renderNode: {
+            [INLINES.ENTRY_HYPERLINK]: node => {
+              return `<a key="${shortid.generate()}" href="${
+                node.data.target.fields.slug['en-GB']
+              }">${node.content[0].value}</a>`
+            },
+          },
+        },
+      },
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
